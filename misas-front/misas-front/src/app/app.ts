c361df82e8fb827493f0ede1ccf8f87de3441d0e
Efrain Serna Gracia <efrain_serna@hotmail.com>
2026-04-08 10:33:00 -0600

    Actualización de App para mostrar componentes

--- misas-front/misas-front/src/app/app.ts
+++ misas-front/misas-front/src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Locacionespage } from './pages/locaciones.page';
+import { Locacionespage } from './pages/locaciones/locaciones.page';
+import { ColoniasPage } from './pages/colonias/colonias.page';
+import { CiudadesPage } from './pages/ciudades/ciudades.page';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
-    Locacionespage
+    Locacionespage,
+    ColoniasPage,
+    CiudadesPage
   ],
   templateUrl: './app.html',
   styleUrl: './app.css'
