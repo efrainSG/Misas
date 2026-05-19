d6bea45a8f60cfdba23656662334b3f13a669bb1
Efrain Serna Gracia <efrain_serna@hotmail.com>
2026-05-19 08:29:52 -0600

    Creational requests

---
+++ misas-api/app/Http/Requests/CreateHorarioRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateHorarioRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'locacionid' => 'required|integer|exists:Locaciones,Id',
+            'diasemana' => 'required|integer|between:0,6',
+            'hora' => 'required|date_format:H:i',
+            'activo' => 'required|boolean',
+            'notas' => 'nullable|string',
+        ];
+    }
+}
