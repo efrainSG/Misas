cfcef9b8f6610bd4902e0c26cce8f5bf2d9b8232
Efrain Serna Gracia <efrain_serna@hotmail.com>
2026-05-19 09:19:19 -0600

    Request para actualizar datos

---
+++ misas-api/app/Http/Requests/UpdateLocacionRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateLocacionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'nombre' => 'required|string|max:150',
+            'direccion' => 'required|string|max:500',
+            'coloniaid' => 'required|integer|exists:Colonias,Id',
+            'telefono' => 'nullable|string|max:20',
+            'tipolocacionid' => 'required|integer|exists:TipoLocaciones,Id'
+        ];
+    }
+}
